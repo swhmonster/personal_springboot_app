63cb0d57b0332c0d98e2a79c7465d7d9bb9e6aee
walter <swh7941@qq.com>
2020-07-11 14:38:49 +0800

    【*】项目搭建

---
+++ source_code/spbt/src/main/java/com/walter/spbt/SpbtApplication.java
@@ -0,0 +1,13 @@
+package com.walter.spbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbtApplication.class, args);
+    }
+
+}
---
+++ source_code/spbt/src/test/java/com/walter/spbt/SpbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.spbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
