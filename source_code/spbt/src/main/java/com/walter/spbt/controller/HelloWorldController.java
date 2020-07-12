869e1551e2110fd27ce8cdabc608379e7ef90d97
walter <swh7941@qq.com>
2020-07-12 19:53:06 +0800

    【*】axios构建

--- source_code/spbt/src/main/java/com/walter/spbt/controller/HelloWorldController.java
+++ source_code/spbt/src/main/java/com/walter/spbt/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.walter.spbt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("helloworld")
+    @RequestMapping("/")
     public String sayHello(){
-        return "hello world";
+        return "static/index";
     }
 }
