540cfacac5f918fe5a0ba5b12d04c099625bd665
walter <swh7941@qq.com>
2020-07-30 21:37:30 +0800

    【*】swagger配置

--- source_code/spbt/src/main/java/com/walter/spbt/controller/HelloWorldController.java
+++ source_code/spbt/src/main/java/com/walter/spbt/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.walter.spbt.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,10 @@ import io.swagger.annotations.Api;
 
 @RestController
 @Api(tags="swagger集成测试，后续作为业务开发")
+@RequestMapping("api")
 public class HelloWorldController {
-    @RequestMapping("/sayhello")
+    @GetMapping("sayhello")
+    @ApiOperation(value = "测试",response = String.class)
     public String sayHello(){
         return "static/index";
     }
