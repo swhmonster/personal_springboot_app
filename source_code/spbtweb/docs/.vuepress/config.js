e0310cfb0387f571fa13eabe07cfd3d8fcc9fdc7
sunwenhao <sunwenhao@thunisoft.com>
2020-08-06 16:33:07 +0800

    【*】md文件打包路径修改

--- source_code/spbtweb/docs/.vuepress/config.js
+++ source_code/spbtweb/docs/.vuepress/config.js
@@ -1,12 +1,18 @@
 module.exports = {
+  dest:'static/',
   title: 'VuePress',
   description: 'the new world',
   home: true,
   heroImage:null,
   heroText: null,
   tagline: null,
-  // actionText: null,
-  // actionLink: null,
-  // features:
-  //   footer:
+  actionText: null,
+  actionLink: null,
+  footer:null,
+  themeConfig: {
+    navbar:false,
+    sidbar:false,
+    search:false,
+    displayAllHeaders: true
+  }
 }
