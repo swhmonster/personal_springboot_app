a8fb5e9df82af421f21d9bd8938c33196dee3f9e
walter <swh7941@qq.com>
2020-08-08 21:56:17 +0800

    【*】代码暂存

--- source_code/spbt/src/main/java/com/walter/spbt/controller/MdToHtmlController.java
+++ source_code/spbt/src/main/java/com/walter/spbt/controller/MdToHtmlController.java
@@ -1,6 +1,9 @@
 package com.walter.spbt.controller;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,14 +41,19 @@ public class MdToHtmlController {
     @GetMapping("mdfile")
     @ResponseBody
     public SpbtResponseEntity getMarkdownFile() {
-        File file = new File("src/main/resources/static/static/vuepress/mdfiles");
-        List<File> fileList = Arrays.asList(file.listFiles());
-        String hostAddress =FILE_PATH;
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        this.getClass().getClassLoader().getResourceAsStream("/static/static/vuepress/mdfiles/")));
+        List<String> fileNameList=new ArrayList<>();
+        String line;
+        while ((line = in.readLine()) != null){
+            fileNameList.add(line);
+        }
         List<Map<String, String>> linkList = new ArrayList<>();
-        fileList.forEach(e -> {
+        fileNameList.forEach(e -> {
             Map<String, String> map = new HashMap<>();
-            map.put(FILE_NAME, e.getName());
-            map.put(FILE_ADRESS, hostAddress + e.getName());
+            map.put(FILE_NAME, e);
+            map.put(FILE_ADRESS, FILE_PATH + e);
             linkList.add(map);
         });
         return ResponseUtils.successResponse(linkList);
